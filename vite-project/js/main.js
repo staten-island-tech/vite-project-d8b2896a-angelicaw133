cc4b98c47fa05f8a9b6d15269d28f208e31aacc9
angelicaw133 <angelicawo426@gmail.com>
2023-11-14 10:14:20 -0500

    sfd

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -3,3 +3,8 @@ import '../css/style.css'
 import {countries} from './sell'
 
 console.log(countries);
+
+countries.forEach(country => console.log(country.name))
+
+const asia = countries.filter(country = country.continent === "Asia")
+console.log(asia);
--- vite-project/js/sell.js
+++ vite-project/js/sell.js
@@ -4,25 +4,30 @@ const countries = [
         name : "USA",
         price : "$5",
         continent: "North America",
+        image: "https://cdn.britannica.com/33/4833-050-F6E415FE/Flag-United-States-of-America.jpg",
     },
     {
         name : "China",
         price : "$5",
         continent: "Asia",
+        image: "https://cdn.britannica.com/90/7490-050-5D33348F/Flag-China.jpg?w=400&h=235&c=crop"
     },
     {
         name : "Brazil",
         price : "$5",
         continent: "South America",
+        image: "https://upload.wikimedia.org/wikipedia/en/thumb/0/05/Flag_of_Brazil.svg/640px-Flag_of_Brazil.svg.png",
     },
     {
         name : "Egypt",
         price : "$5",
         continent: "Africa",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Flag_of_Egypt_%281972%E2%80%931984%29.svg/220px-Flag_of_Egypt_%281972%E2%80%931984%29.svg.png"
     },
     {
         name : "France",
         price : "$5",
         continent: "Europe",
+        image: "https://upload.wikimedia.org/wikipedia/en/thumb/c/c3/Flag_of_France.svg/2560px-Flag_of_France.svg.png",
     }
 ]
