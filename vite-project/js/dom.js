15bd094c87f6fb88c9b7a7b010db13e3d228f31e
angelicaw133 <angelicawo426@gmail.com>
2023-11-15 10:10:01 -0500

    dfs

---
+++ vite-project/js/dom.js
@@ -0,0 +1,8 @@
+const DOMselectors = {
+    card : document.querySelector(".card"),
+    card_image : document.querySelector(".cardimg"),
+    buttons : document.querySelector(".button"),
+    name : document.querySelector(".name"),
+    price : document.querySelector(".price"),
+}
+
--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -2,9 +2,18 @@ import '../css/style.css'
 
 import {countries} from './sell'
 
-console.log(countries);
 
-countries.forEach(country => console.log(country.name))
-
-const asia = countries.filter(country = country.continent === "Asia")
+const asia = countries.filter((country) => country.continent === "Asia")
 console.log(asia);
+
+const north_america = countries.filter((country) => country.continent === "North America")
+console.log(north_america);
+
+const south_america = countries.filter((country) => country.continent === "South America")
+console.log(south_america);
+
+const africa = countries.filter((country) => country.continent === "Africa")
+console.log(africa);
+
+const europe = countries.filter((country) => country.continent === "Europe")
+console.log(europe);
